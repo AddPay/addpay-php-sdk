ebe8ca5c7c882a91d3bda28b74b5ac4fc3f0abaa
Stephen Lake <stephen@closurecode.com>
2018-02-01 12:50:25 +0200

    Remove private API because its not being utilised correctly

--- core/Layers/PrivateAPI.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-class PrivateAPI extends AddPay\Foundation\Protocol\API\PrivateAPI {}
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,5 +2,4 @@
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/Layers/OpenAPI.php');
 require_once(__DIR__ . '/Layers/PublicAPI.php');
-require_once(__DIR__ . '/Layers/PrivateAPI.php');
 require_once(__DIR__ . '/helpers.php');
